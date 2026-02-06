123801e8ef3015709adfaf3245ec5cd4dda7f3cc
norbekhamidov <norbekha@gmail.com>
2026-02-06 11:33:13 +0500

    Uzbek Language Seeder updated

--- database/seeders/LanguageSeeder.php
+++ database/seeders/LanguageSeeder.php
@@ -2,32 +2,34 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\AvailableLanguage;
 
 class LanguageSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
+        // SQLite uchun
+        AvailableLanguage::query()->delete();
+
         $languages = [
-            ['lang_name' => 'English'],
-            ['lang_name' => 'Spanish'],
-            ['lang_name' => 'French'],
-            ['lang_name' => 'German'],
-            ['lang_name' => 'Italian'],
-            ['lang_name' => 'Portuguese'],
-            ['lang_name' => 'Russian'],
-            ['lang_name' => 'Chinese'],
-            ['lang_name' => 'Japanese'],
-            ['lang_name' => 'Korean'],
-            ['lang_name' => 'Uzbek'],
+            ['lang_name' => 'O‘zbek tili'],
+            ['lang_name' => 'Ingliz tili'],
+            ['lang_name' => 'Rus tili'],
+            ['lang_name' => 'Ispan tili'],
+            ['lang_name' => 'Fransuz tili'],
+            ['lang_name' => 'Nemis tili'],
+            ['lang_name' => 'Arab tili'],
+            ['lang_name' => 'Xitoy tili'],
+            ['lang_name' => 'Yapon tili'],
+            ['lang_name' => 'Koreys tili'],
+            ['lang_name' => 'Turk tili'],
+            ['lang_name' => 'Portugal tili'],
+            ['lang_name' => 'Italyan tili'],
+            ['lang_name' => 'Hindi tili'],
+            ['lang_name' => 'Fors tili'],
         ];
 
-        foreach ($languages as $language) {
-            \App\Models\AvailableLanguage::create($language);
-        }
+        AvailableLanguage::insert($languages);
     }
 }
